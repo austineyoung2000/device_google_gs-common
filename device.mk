a4253406a63d3ebf922c13bb89849e8f8eea1218
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-10-13 13:50:00 +0800

    optional inclusion of google camera
    Change-Id: Ibf7b9b4dade843c4a8ca7a624e2377f44edbc269
    (cherry picked from commit cb7beef4b858211ccbe0cf925681f59bd61806ef)
    (cherry picked from commit f9c6f225a968d5aa0dae253c063ad2df29679b43)
    (cherry picked from commit 334d0621d81a714e58fa2bd3382b4441af86824e)

--- device.mk
+++ device.mk
@@ -18,6 +18,8 @@ ifeq (,$(filter true, $(PRODUCT_WITHOUT_TTS_VOICE_PACKS)))
 include device/google/gs-common/tts/voice_packs.mk
 endif
 
+-include vendor/google/camera/config.mk
+
 HBM_SUPPORTED := true
 HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
 TORCH_STR_SUPPORTED := true
